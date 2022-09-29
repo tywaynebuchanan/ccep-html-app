48ff9b8828e301d503b77fe48ba4a93ebaaac3eb
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2022-09-28 19:44:54 -0500

    made site mobile friendly

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// let close_btn = document.getElementById("menu-close");
+// let  menu_btn = document.getElementById("menu-btn");
+// let navigation = document.getElementById("navbar");
+
+// menu_btn.addEventListener("click",()=>{
+//   navigation.classList.remove("active");
+// })
+// close_btn.addEventListener("click",()=>{
+//     navigation.classList.remove("active");
+// })
+
