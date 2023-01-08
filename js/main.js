6cf6228a249c06335ce9f8d765e6282c0d3ee1f0
Tywayne Buchanan <tywaynebuchanan@gmail.com>
2023-01-08 14:53:00 -0500

    made changes to layout and added pages

--- js/main.js
+++ js/main.js
@@ -1,11 +1,1 @@
-// let close_btn = document.getElementById("menu-close");
-// let  menu_btn = document.getElementById("menu-btn");
-// let navigation = document.getElementById("navbar");
-
-// menu_btn.addEventListener("click",()=>{
-//   navigation.classList.remove("active");
-// })
-// close_btn.addEventListener("click",()=>{
-//     navigation.classList.remove("active");
-// })
-
+document.alert("Hello world");
